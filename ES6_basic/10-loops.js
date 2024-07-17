5b562ae05666cdd8c9e160dd713fe586d4c75446
nuocmam75009 <sayhi.kiseiju@gmail.com>
2024-07-17 12:06:05 +0200

    yes

--- ES6_basic/10-loops.js
+++ ES6_basic/10-loops.js
@@ -1,7 +1,7 @@
 export default function appendToEachArrayValue(array, appendString) {
     for (let value of array) {
-        const array2 = value + appendString;
-        array.push(array2);
+        value = appendString + value;
     }
-    return array.slice();
+
+    return array;
 }
