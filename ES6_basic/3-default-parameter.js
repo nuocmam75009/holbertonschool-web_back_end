32ce023972ebc1a50e4013f1ad60cd27b27e79d6
nuocmam75009 <sayhi.kiseiju@gmail.com>
2024-07-17 11:06:59 +0200

    update

--- ES6_basic/3-default-parameter.js
+++ ES6_basic/3-default-parameter.js
@@ -1,4 +1,5 @@
-export default getSumOfHoods;
 const getSumOfHoods = (initialNumber,
     expansion1989 = 89,
     expansion2019 = 19) => initialNumber + expansion1989 + expansion2019;
+
+export default getSumOfHoods;
