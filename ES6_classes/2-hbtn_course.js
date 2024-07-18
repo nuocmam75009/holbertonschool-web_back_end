e1926e51f036cbd2689ce550294648600fa92b42
nuocmam75009 <sayhi.kiseiju@gmail.com>
2024-07-18 10:41:50 +0200

    task3

--- ES6_classes/2-hbtn_course.js
+++ ES6_classes/2-hbtn_course.js
@@ -1,37 +1,44 @@
-class HolbertonCourse {
+export default class HolbertonCourse {
     constructor(name, length, students) {
-        this._name = typeof name === 'string' ? name : '';
-        this._length = typeof length === 'number' ? length : 0;
-        this._students = Array.isArray(students) ? students : [];
-    }
-
-    get name() {
-        return this._name;
+      if (typeof name !== 'string') throw TypeError('name must be a String');
+      if (typeof length !== 'number') throw TypeError('length must be a Number');
+      if (!Array.isArray(students)) throw TypeError('students must be an Array');
+      students.forEach((student) => {
+        if (typeof student !== 'string') throw TypeError('student must be a String');
+      });
+      this._name = name;
+      this._length = length;
+      this._students = students;
     }
 
     set name(newName) {
-        if (typeof newName === 'string') {
-            this._name = newName;
-        }
+      if (typeof newName !== 'string') throw TypeError('name must be a String');
+      this._name = newName;
     }
 
-    get length() {
-        return this._length;
+    get name() {
+      return this._name;
     }
 
     set length(newLength) {
-        if (typeof newLength === 'number') {
-            this._length = newLength;
-        }
+      if (typeof newLength !== 'number') throw TypeError('length must be a Number');
+      this._length = newLength;
     }
 
-    get students() {
-        return this._students;
+    get length() {
+      return this._length;
     }
 
     set students(newStudents) {
-        if (Array.isArray(newStudents)) {
-            this._students = newStudents;
-        }
+      if (!Array.isArray(newStudents)) throw TypeError('students must be an Array');
+      newStudents.forEach((student) => {
+        if (typeof student !== 'string') throw TypeError('student must be a String');
+      });
+      this._students = newStudents;
     }
-}
+
+    get students() {
+      return this._students;
+    }
+  }
+  
