72b54237c0b37089f14a42f0888c8791a54487ea
nuocmam75009 <sayhi.kiseiju@gmail.com>
2024-07-19 11:28:06 +0200

    fix + add gitignore

--- ES6_basic/10-loops.js
+++ ES6_basic/10-loops.js
@@ -1,7 +1,7 @@
 export default function appendToEachArrayValue(array, appendString) {
-    for (let value of array) {
-      const newValue = appendString + value;
-      array.push(newValue);
-    }
-    return array.slice();
+  for (const value of array) {
+    const newValue = appendString + value;
+    array.push(newValue);
   }
+  return array.slice();
+}
