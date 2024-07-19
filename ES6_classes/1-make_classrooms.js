a57979b9bc7bc30b52876ae996deab22d82b119a
nuocmam75009 <sayhi.kiseiju@gmail.com>
2024-07-19 11:52:43 +0200

    fix + gitignore

--- ES6_classes/1-make_classrooms.js
+++ ES6_classes/1-make_classrooms.js
@@ -1,8 +1,8 @@
-import ClassRoom from './0-classrooms.js';
+import ClassRoom from './0-classroom';
 
 const initializeRooms = () => {
-    const array = [19, 20, 34];
-    return array.map((n) => new ClassRoom(n));
+  const array = [19, 20, 34];
+  return array.map((n) => new ClassRoom(n));
 };
 
 export default initializeRooms;
