685d2be3c628bf96c0e7b273f7070d1d6bb47027
nuocmam75009 <sayhi.kiseiju@gmail.com>
2024-07-26 10:11:24 +0200

    two first tasks

---
+++ ES6_promise/0-promise.js
@@ -0,0 +1,10 @@
+export default function getResponseFromAPI() {
+  return new Promise((resolve, reject) => {
+    const success = true;
+    if (success) {
+      resolve('Stuff worked!');
+    } else {
+      reject(Error('It broke'));
+    }
+  });
+}
