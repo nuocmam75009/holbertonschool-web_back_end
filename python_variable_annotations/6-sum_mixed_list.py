e31713ecc47b0483d63f48c75d1de59870cf873a
nuocmam75009 <sayhi.kiseiju@gmail.com>
2024-08-14 10:44:06 +0200

    push the whole project

---
+++ python_variable_annotations/6-sum_mixed_list.py
@@ -0,0 +1,4 @@
+from typing import List, Union
+
+def sum_mixed_list(mxd_list: List[Union[int, float]]) -> float:
+    return sum(mxd_list)
---
+++ python_variable_annotations/README.md
@@ -0,0 +1,1 @@
+Project about variable annotations in Python
