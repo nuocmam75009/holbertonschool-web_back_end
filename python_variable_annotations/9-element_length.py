cddef9ee81ebbd2f4bbe88f0c5afc27669c70460
nuocmam75009 <sayhi.kiseiju@gmail.com>
2024-08-16 20:18:25 +0200

    fix

--- python_variable_annotations/9-element_length.py
+++ python_variable_annotations/9-element_length.py
@@ -2,10 +2,10 @@
 
 # Task 9
 
-from typing import List, Tuple
+from typing import List, Tuple, Sequence, Iterable
 
 
-def element_length(lst: List[str]) -> List[Tuple[str, int]]:
+def element_length(lst: Iterable[Sequence]) -> List[Tuple[Sequence, int]]:
     #  Return a list of tuples with the first element as the string and the
     #  second element as the length of the string
     return [(i, len(i)) for i in lst]
