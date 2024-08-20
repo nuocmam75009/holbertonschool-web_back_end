c0e26f45322d59242a7260fc70aa81646b05482d
nuocmam75009 <sayhi.kiseiju@gmail.com>
2024-08-20 10:11:23 +0200

    exercice

---
+++ python_async_comprehension/0-async_generator.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+
+# Task 0
+
+import asyncio
+import random
+
+
+async def async_generator():
+    # This function will return 10 random numbers between 0 and 10
+    for _ in range(10):
+        await asyncio.sleep(1)
+        yield random.uniform(0, 10)
