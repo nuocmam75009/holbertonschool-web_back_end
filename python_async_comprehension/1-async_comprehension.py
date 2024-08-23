e74b432087634002de81250657e8ffaea55ee506
nuocmam75009 <sayhi.kiseiju@gmail.com>
2024-08-23 23:38:21 +0200

    fix

--- python_async_comprehension/1-async_comprehension.py
+++ python_async_comprehension/1-async_comprehension.py
@@ -1,15 +1,15 @@
 #!/usr/bin/env python3
 
-"""Async comprehension"""
+"""1. Async Comprehensions"""
 
 import asyncio
+import random
 from typing import List
-
 
 async_generator = __import__('0-async_generator').async_generator
 
 
 async def async_comprehension() -> List[float]:
-    """# Collect 10 random numbers using an
-    # async comprehension over async_generator"""
-    return [i async for i in async_generator()]
+    """coroutine collect 10 random numbers"""
+    random_number = [i async for i in async_generator()]
+    return random_number
--- python_async_comprehension/2-measure_runtime.py
+++ python_async_comprehension/2-measure_runtime.py
@@ -1,20 +1,18 @@
 #!/usr/bin/env python3
 
-
-"""Measure runtime"""
+"""2. Run time for four parallel comprehensions"""
 
 import asyncio
-
 
 async_comprehension = __import__('1-async_comprehension').async_comprehension
 
 
 async def measure_runtime() -> float:
-    """  Measure the runtime of async_comprehension executed 4 times in parallel"""
-    start_time = asyncio.get_event_loop().time()
-    await asyncio.gather(*(async_comprehension() for _ in range(4)))
-    end_time = asyncio.get_event_loop().time()
-    return end_time - start_time
-
-if __name__ == "__main__":
-    print(asyncio.run(measure_runtime()))
+    """measure total runtime"""
+    begin = asyncio.get_event_loop().time()
+    await asyncio.gather(async_comprehension(),
+                         async_comprehension(),
+                         async_comprehension(),
+                         async_comprehension())
+    end = asyncio.get_event_loop().time()
+    return end - begin
