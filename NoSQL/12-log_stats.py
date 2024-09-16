c61981acfab30deaac8035266f7af51a98341618
nuocmam75009 <sayhi.kiseiju@gmail.com>
2024-09-16 15:29:18 +0200

    initial commit

--- NoSQL/12-log_stats.py
+++ NoSQL/12-log_stats.py
@@ -2,33 +2,32 @@
 
 """12. Log stats"""
 
+
 from pymongo import MongoClient
 
 
-def nginx_stats():
-    """provides some stats about Nginx logs"""
-    client = MongoClient('mongodb://localhost:27017/')
+# Connect to MongoDB
+client = MongoClient()
+db = client.logs
+collection = db.nginx
 
-    db = client.logs
-    collection = db.nginx
+# Get the total number of documents in the collection
+total_logs = collection.count_documents({})
 
-    total_logs = collection.count_documents({})
-    print(f'{total_logs} logs')
+# Print the number of logs
+print(f"{total_logs} logs where {total_logs} is the number of documents in this collection")
 
-    print('Methods:')
-    methods = ["GET", "POST", "PUT", "PATCH", "DELETE"]
-    for method in methods:
-        count = collection.count_documents({"method": method})
-        print(f'\tmethod {method}: {count}')
+# Get the number of documents with each method
+methods = ["GET", "POST", "PUT", "PATCH", "DELETE"]
+method_counts = [collection.count_documents({"method": method}) for method in methods]
 
-    status_check_query = {
-        "method": "GET",
-        "path": "/status"
-    }
-    status_check_count = collection.count_documents(status_check_query)
-    print(f'{status_check_count} status check')
+# Print the number of documents with each method
+print("Methods:")
+for method, count in zip(methods, method_counts):
+    print(f"\t{count} {method}")
 
+# Get the number of documents with method=GET and path=/status
+get_status_count = collection.count_documents({"method": "GET", "path": "/status"})
 
-if __name__ == "__main__":
-    nginx_stats()
-    
+# Print the number of documents with method=GET and path=/status
+print(f"{get_status_count} logs where method=GET and path=/status")
---
+++ Node_JS_basic/README.md
@@ -0,0 +1,1 @@
+Learn about Node.js
